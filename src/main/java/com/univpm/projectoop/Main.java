78f56e7aadece9001188781aa564ac040c8a76c0
eugeniodandrea <eugenio.dandrea@outlook.it>
2019-07-03 11:52:37 +0200

    Create JSON parser and downloader

---
+++ src/main/java/com/univpm/projectoop/Main.java
@@ -0,0 +1,24 @@
+package com.univpm.projectoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			Connect Connection = new Connect();
+			Connection.startConnect();
+			JSONManager handler = new JSONManager(Connection.getData());
+			handler.createCSV();
+		} catch (NullPointerException e){
+			System.out.println("Error");
+			e.printStackTrace();
+		}
+		//SpringApplication.run(Main.class, args);
+	}
+}
+
