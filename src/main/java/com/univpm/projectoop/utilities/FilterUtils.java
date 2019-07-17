84240c1ec9eccecbbe0896e281ffe88acabd607f
Michele997 <michele.marchetti97@hotmail.it>
2019-07-17 16:46:18 +0200

    Update check method on FilterUtils class

--- src/main/java/com/univpm/projectoop/utilities/FilterUtils.java
+++ src/main/java/com/univpm/projectoop/utilities/FilterUtils.java
@@ -3,21 +3,62 @@ package com.univpm.projectoop.utilities;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class FilterUtils<T> {
-    public static boolean check(Object value, String operator, Object th) {
-        if (th instanceof Number && value instanceof Number) {
-            Double thC = ((Number) th).doubleValue();
-            Double valuec = ((Number) value).doubleValue();
-            if (operator.equals("=="))
-                return value.equals(th);
-            else if (operator.equals(">"))
-                return valuec > thC;
-            else if (operator.equals("<"))
-                return valuec < thC;
-        } else if (th instanceof String && value instanceof String)
-            return value.equals(th);
+    public static boolean check(Object value, String operator, Object... obj) {
+
+        if (value != null) {
+
+            if (obj.length == 1 && obj[0] instanceof Number && value instanceof Number) {
+
+                Float objF = ((Number) obj[0]).floatValue();
+                Float valueF = ((Number) value).floatValue();
+
+                if (operator.equals("$eq")) {
+                    Float prova = Float.valueOf(String.valueOf(valueF));
+                    Float prova1 = Float.valueOf(String.valueOf(objF));
+                    boolean b = prova.equals(prova1);
+                    return b;
+                } else if (operator.equals("$not"))
+                    return !value.equals(obj[0]);
+                else if (operator.equals("$gt"))
+                    return valueF > objF;
+                else if (operator.equals("$gte"))
+                    return valueF >= objF;
+                else if (operator.equals("$lt"))
+                    return valueF < objF;
+                else if (operator.equals("$lte"))
+                    return valueF <= objF;
+
+            } else if (obj.length == 1 && obj[0] instanceof String && value instanceof String) {
+
+                if (operator.equals("$eq") || operator.equals("$in"))
+                    return ((String) value).equals((String) obj[0]);
+                else if (operator.equals("$not") || operator.equals("$nin")) return !value.equals(obj[0]);
+
+            } else if (obj.length > 1) {
+
+                if (operator.equals("$bt")) {
+                    if (obj.length == 2 && obj[0] instanceof Number && obj[1] instanceof Number) {
+                        Float min = ((Number) obj[0]).floatValue();
+                        Float max = ((Number) obj[1]).floatValue();
+                        Float valueF = ((Number) value).floatValue();
+                        return valueF >= min && valueF <= max;
+                    }
+                } else if (operator.equals("$in")){
+                    boolean b = Arrays.asList(obj).contains(value);
+                    return b;
+                }
+
+                else if (operator.equals("$nin")) {
+                    boolean b = !Arrays.asList(obj).contains(value);
+                    return b;
+                }
+            }
+        }
+
         return false;
     }
 
