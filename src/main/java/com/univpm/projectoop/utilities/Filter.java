cb8a2301d9ed7b7e9709d42c1c7d262b3ee6c79e
eugeniodandrea <eugenio.dandrea@outlook.it>
2019-07-17 16:43:40 +0200

    Collection updated

--- src/main/java/com/univpm/projectoop/utilities/Filter.java
+++ src/main/java/com/univpm/projectoop/utilities/Filter.java
@@ -1,7 +1,8 @@
 package com.univpm.projectoop.utilities;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 
 public interface Filter<E,T> {
-    abstract Collection<E> filterField(String fieldName, String operator, T value);
+    abstract Collection<E> filterField(String fieldName, String operator, T... value) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException;
 }
